b3449470ae5ddc0449ae5e299eff0f895d915e66
ACE43-dev <hpomen43@gmail.com>
2025-12-03 08:14:19 +0530

    removed wrong readme.md

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,65 @@
 // import Gallery from './Gallery.jsx'
 // import Profile from './Profile.jsx'
 // import './App.css' 
-const people = [
-        'Creola Katherine Johnson: mathematician',
-        'Mario José Molina-Pasquel Henríquez: chemist',
-        'Mohammad Abdus Salam: physicist',
-        'Percy Lavon Julian: chemist',
-        'Subrahmanyan Chandrasekhar: astrophysicist'
-    ];
+import { people } from "./data.js";
+import { getImageUrl } from "./utils.js";
+const chemist = people.filter((n) => n.profession === 'chemist')
+
+
+
 function App() {
-    
-    const list=people.map(n=><li>{n}</li>)
-    return(
-        <>
-            <ul>{list}</ul>
-        </>
+
+    const listItem = chemist.map((n) =>
+        <div className="flex">
+            <div>
+                <img
+                    src={getImageUrl(n)}
+                    alt={n.name}
+                />
+            </div>
+            <div>
+                <li>
+                    <p>
+                        <b>{n.name}:</b>
+                        {' ' + n.profession + ' '}
+                        known for {" " + n.accomplishment + " "}
+                    </p>
+                </li>
+            </div>
+        </div>
     )
+    return (
+        <ul>{listItem}</ul>
+    )
+
 }
 
 
 
 export default App;
+
+function GradeCard({ marks }) {
+  let grade;
+
+  if (marks >= 90) grade = "A";
+  else if (marks >= 75) grade = "B";
+  else if (marks >= 50) grade = "C";
+  else if (marks >= 35) grade = "D";
+  else grade = "F";
+
+  if (marks >= 90) css = "top";
+  else if (marks >= 75) css = "mid";
+  else if (marks >= 50) css = "mid";
+  else if (marks >= 35) css = "mid";
+  else css = "low";
+
+  const status = marks >= 35 ? "Pass" : "Fail";
+
+  return (
+    <ul>
+      <li >Marks: {marks}/100</li>
+      <li className={css}>Grade: {grade}</li>
+      <li>Status: {status}</li>
+    </ul>
+  );
+}
